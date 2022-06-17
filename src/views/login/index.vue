a97060e6d55b477dc85f2323d1f1937636f099ca
joker <lee_yemeng@163.com>
2022-06-17 10:24:16 +0800

    fix: 修改配置

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,23 +1,17 @@
 <template>
-  <div class="text">login</div>
-  <div>{{ store.name }}</div>
-  <div>{{ store.nameLen }}</div>
-  <div>{{ letter }}</div>
-  <hello-world msg="这是一个测试" count="3"></hello-world>
+  <div></div>
 </template>
-<script setup lang="ts">
-import { useMainStore } from "@/store";
-
-import HelloWorld from "@/components/HelloWorld.vue";
-import { computed } from "vue";
-
-import { watch } from "vue";
-const store = useMainStore();
-const letter = computed(() => store.name.toLocaleLowerCase());
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+export default defineComponent({
+  name: "",
+  components: {},
+  props: {},
+  setup() {
+    const data = reactive({});
+    const refData = toRefs(data);
+    return { ...refData };
+  },
+});
 </script>
-
-<style scoped lang="scss">
-.text {
-  color: green;
-}
-</style>
+<style scoped lang="less"></style>
